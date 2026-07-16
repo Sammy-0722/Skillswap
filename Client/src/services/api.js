96aee3ee5daafb8a9a884300ed863f69e4de384a
Sammy-0722 <samrana2204@gmail.com>
2026-07-16 19:04:38 +0530

    backend link

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://skillswap-hkg7.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
